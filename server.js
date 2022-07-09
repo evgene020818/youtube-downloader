7a8acf05ba170ae1660af9049a348cd3cf952ed0
evgene020818 <evgene.020818@gmail.com>
2022-07-09 19:54:15 +0300

    Updating server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use('/api', require('./routes/root'));
 app.use(express.static(path.join(__dirname, 'client/build')));
 // Handle React routing, return all requests to React app
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'Client/build', 'index.html'));
 });
 
 app.all('*', (req, res) => {
