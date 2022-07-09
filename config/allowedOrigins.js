83ae74aa0a8a116d76606ae3c3f8a333b9aea802
evgene020818 <evgene.020818@gmail.com>
2022-07-09 22:11:57 +0300

    Updating files...

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3500',
     'http://localhost:3000',
-    'http://localhost',
+    'http://localhost:' + process.env.PORT,
     'https://evgene-youtube-downloader-1.herokuapp.com/',
 ];
 
